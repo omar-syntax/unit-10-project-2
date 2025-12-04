033c7b8cc3fc12fdb2f845994ad440cb830623b7
Omar-1210 <mmr256366@gmail.com>
2025-12-05 01:30:53 +0200

    adding an image slider to offers section

--- main.js
+++ main.js
@@ -1,3 +1,19 @@
-const home = document.getElementById('main-home');
-home.textContent='maruoma';
-home.className = 'maruoma banotty';
+const offers = document.getElementById('offers-items');
+const next = document.getElementById('next');
+const prev = document.getElementById('prev');
+let index = 0;
+
+function slide(i){
+    const total = offers.children.length;
+    index = (index + i + total) % total;
+    offers.style.transform = `translate(-${index * 100}%)`;
+    offers.style.transition = `all 1s`;
+}
+
+
+next.onclick = () => slide(1); 
+prev.onclick = () => slide(-1); 
+setInterval(() => slide(1), 5000); 
+
+// ----------------------------------------------------------
+
